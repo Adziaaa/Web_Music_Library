05b034d9cd11a760a15c0e17f8ab9d1bcc69f057
Chirita Marius <chiritamarius818@gmail.com>
2024-11-06 14:49:38 +0100

    new commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,21 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-class ProfileController
-{
-    public function show($id)
-    {
-        $user = User::findOrFail($id);
-        return view('profile.show', compact('user'));
-    }
-}
-<?php
-
-namespace App\Http\Controllers;
-
 use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -85,5 +70,11 @@ class ProfileController extends Controller
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('profile.show', compact('user'));
     }
 }
