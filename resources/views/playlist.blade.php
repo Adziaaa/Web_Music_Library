3e251dca982ca4a994347f962b8cd1fb99ae27aa
Adriana <adrianajaworska1717@gmail.com>
2024-12-09 21:10:44 +0100

    last commit

--- resources/views/playlist.blade.php
+++ resources/views/playlist.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Extension - Music Player</title>
     <link rel="stylesheet" href="style1.css">
-    <!-- Bootstrap Icons CDN Link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
--- resources/views/read.blade.php
+++ resources/views/read.blade.php
@@ -1,6 +1,5 @@
 <x-app-layout>
     <link rel="stylesheet" href="{{ asset('css/style1.css') }}">
-    <!-- Bootstrap Icons CDN Link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
