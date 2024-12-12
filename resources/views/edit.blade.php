c6fd5cba9c132f46e527dde9005d9a8beeefb88c
Adriana <adrianajaworska1717@gmail.com>
2024-12-12 17:35:56 +0100

    changes 1/2

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -10,7 +10,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
+
     <title>SCHEDULER</title>
+
 </head>
 
 <body>
