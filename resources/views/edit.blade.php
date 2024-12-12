d38d847b6a956aa50b201535a9d103f2cf2fb1a7
Adriana <adrianajaworska1717@gmail.com>
2024-12-12 17:38:27 +0100

    done

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,86 +1,61 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/support.css') }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+<x-app-layout>
     <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
 
-    <title>SCHEDULER</title>
+    <body>
 
-</head>
 
-<body>
-    <div class="container">
-        <div class="logo">
-            <img src="https://img.icons8.com/quill/100/228BE6/galaxy.png" alt="Home" />
-            <h1><a href="/main">Atari800</a></h1>
-        </div>
-        <div class="profile-menu">
-            <img src="{{ asset('images/profile.png') }}" alt="Profile" onclick="toggleDropdown()">
-            <div class="dropdown-menu" id="dropdownMenu">
-                <a href="/profilepage">Profile</a>
-                <a href="/settings">Settings</a>
+        <div class="scheduler">
+            <h3 class="title_alarm">Update Alarm</h3>
+            <div class="form_alarm">
+                <form action="{{ route('update', ['id' => $playlist->id]) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <input type="text" name="name" value="{{ old('name', $playlist->name) }}" required>
+                    <input type="text" name="duration" value="{{ old('duration', $playlist->duration) }}" required>
+                    <input type="text" name="genre" value="{{ old('genre', $playlist->genre) }}" required>
+                    <button type="submit">Update</button>
+                </form>
+
+
+
             </div>
         </div>
-    </div>
 
-    <div class="scheduler">
-        <h3 class="title_alarm">Update Alarm</h3>
-        <div class="form_alarm">
-            <form action="{{ route('update', ['id' => $playlist->id]) }}" method="POST">
-                @csrf
-                @method('PUT')
-                <input type="text" name="name" value="{{ old('name', $playlist->name) }}" required>
-                <input type="text" name="duration" value="{{ old('duration', $playlist->duration) }}" required>
-                <input type="text" name="genre" value="{{ old('genre', $playlist->genre) }}" required>
-                <button type="submit">Update</button>
-            </form>
-
-
-
+        <div class="bottom">
+            <div class="bottom-section">
+                <h4>Company</h4>
+                <ul>
+                    <li><a href="/about">About us</a></li>
+                    <li><a href="#">Privacy policy</a></li>
+                    <li><a href="#">Affiliate program</a></li>
+                </ul>
+            </div>
+            <div class="bottom-section">
+                <h4>Help</h4>
+                <ul>
+                    <li><a href="#">Q&A</a></li>
+                    <li><a href="#">Sign up</a></li>
+                </ul>
+            </div>
+            <div class="bottom-section">
+                <h4>Contact us</h4>
+                <ul>
+                    <li>Alsion 2, 6400 Sønderborg</li>
+                    <li>Telephone: 6550 1160</li>
+                </ul>
+            </div>
         </div>
-    </div>
 
-    <div class="bottom">
-        <div class="bottom-section">
-            <h4>Company</h4>
-            <ul>
-                <li><a href="/about">About us</a></li>
-                <li><a href="#">Privacy policy</a></li>
-                <li><a href="#">Affiliate program</a></li>
-            </ul>
+        <div class="end">
+            <footer>
+                <marquee>
+                    <p>©2024 Made by Group 7 | All Rights Reserved</p>
+                </marquee>
+            </footer>
         </div>
-        <div class="bottom-section">
-            <h4>Help</h4>
-            <ul>
-                <li><a href="#">Q&A</a></li>
-                <li><a href="#">Sign up</a></li>
-            </ul>
-        </div>
-        <div class="bottom-section">
-            <h4>Contact us</h4>
-            <ul>
-                <li>Alsion 2, 6400 Sønderborg</li>
-                <li>Telephone: 6550 1160</li>
-            </ul>
-        </div>
-    </div>
+    </body>
+</x-app-layout>
 
-    <div class="end">
-        <footer>
-            <marquee>
-                <p>©2024 Made by Group 7 | All Rights Reserved</p>
-            </marquee>
-        </footer>
-    </div>
 
-</body>
 
 </html>
