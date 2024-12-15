a15248bd61994f6fa26b9f47af963f39f616b0ff
kramoda <pavliukiana5@gmail.com>
2024-12-15 19:56:47 +0100

    minor changes

--- public/js/future.js
+++ public/js/future.js
@@ -1,5 +1,5 @@
 var points = [],
-    velocity2 = 5, // velocity squared
+    velocity2 = 5, 
     canvas = 
 document.getElementById('container'),
   context = canvas.getContext('2d'),
@@ -11,11 +11,9 @@ document.getElementById('container'),
 init();
 
 function init() {
-  // create points
   for (var i = 0; i<numberOfPoints; i++) {
     createPoint();
   }
-  // create connections
   for (var i = 0, l=points.length; i<l; i++) {
     var point = points[i];
     if(i == 0) {
@@ -24,8 +22,7 @@ function init() {
       points[i].buddy = points[i-1];
     }
   }
-  
-  // animate
+
   animate();
 }
 
@@ -33,10 +30,8 @@ function createPoint() {
   var point = {}, vx2, vy2;
   point.x = Math.random()*boundaryX;
   point.y = Math.random()*boundaryY;
-  // random vx 
   point.vx = (Math.floor(Math.random())*2-1)*Math.random();
   vx2 = Math.pow(point.vx, 2);
-  // vy^2 = velocity^2 - vx^2
   vy2 = velocity2 - vx2;
   point.vy = Math.sqrt(vy2) * (Math.random()*2-1);
   points.push(point);
@@ -47,13 +42,11 @@ function resetVelocity(point, axis, dir) {
   if(axis == 'x') {
     point.vx = dir*Math.random();  
     vx2 = Math.pow(point.vx, 2);
-  // vy^2 = velocity^2 - vx^2
   vy2 = velocity2 - vx2;
   point.vy = Math.sqrt(vy2) * (Math.random()*2-1);
   } else {
     point.vy = dir*Math.random();  
     vy2 = Math.pow(point.vy, 2);
-  // vy^2 = velocity^2 - vx^2
   vx2 = velocity2 - vy2;
   point.vx = Math.sqrt(vx2) * (Math.random()*2-1);
   }
@@ -76,14 +69,11 @@ function drawLine(x1, y1, x2, y2) {
 
 function draw() {
   for(var i =0, l=points.length; i<l; i++) {
-    // circles
     var point = points[i];
     point.x += point.vx;
     point.y += point.vy;
     drawCircle(point.x, point.y);
-    // lines
     drawLine(point.x, point.y, point.buddy.x, point.buddy.y);
-    // check for edge
     if(point.x < 0+radius) {
       resetVelocity(point, 'x', 1);
     } else if(point.x > boundaryX-radius) {
