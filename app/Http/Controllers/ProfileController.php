bac9c5891831213a8ffa63d94824d9f21fef421f
DanielVacas <daniel.vacas.crespo@gmail.com>
2024-12-17 23:34:21 +0100

    WebExtension

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,10 +6,7 @@ use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-<<<<<<< HEAD
-=======
 use Illuminate\Support\Facades\RateLimiter;
->>>>>>> 3de4682 (Daniel Extension. DDOS DOS Bot protection)
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
@@ -26,15 +23,6 @@ class ProfileController extends Controller
     }
 
     /**
-<<<<<<< HEAD
-     * Update the user's profile information.
-     */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
-        $request->validate([
-            'description' => 'nullable|string|max:1000',
-            'notify_on_new_album' => 'boolean'
-=======
      * Update the user's profile information with rate-limiting.
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
@@ -52,7 +40,6 @@ class ProfileController extends Controller
         $request->validate([
             'description' => 'nullable|string|max:1000',
             'notify_on_new_album' => 'boolean',
->>>>>>> 3de4682 (Daniel Extension. DDOS DOS Bot protection)
         ]);
 
         $request->user()->fill($request->validated());
@@ -60,11 +47,6 @@ class ProfileController extends Controller
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
-<<<<<<< HEAD
-        
-=======
-
->>>>>>> 3de4682 (Daniel Extension. DDOS DOS Bot protection)
         $request->user()->update([
             'description' => $request->description,
             'notify_on_new_album' => $request->notify_on_new_album,
@@ -72,17 +54,11 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-<<<<<<< HEAD
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
-    }
-    
-=======
         RateLimiter::hit($throttleKey, 3600); 
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 
->>>>>>> 3de4682 (Daniel Extension. DDOS DOS Bot protection)
     /**
      * Delete the user's account.
      */
@@ -104,12 +80,9 @@ class ProfileController extends Controller
         return Redirect::to('/');
     }
 
-<<<<<<< HEAD
-=======
     /**
      * Show the user's profile.
      */
->>>>>>> 3de4682 (Daniel Extension. DDOS DOS Bot protection)
     public function show($id)
     {
         $user = User::findOrFail($id);
